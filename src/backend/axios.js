093f6a66823eb4202c1298e21561b995fa37ffb7
Wojciech Mista <wojciech.mista@hotmail.com>
2019-02-07 22:15:07 +0100

    change production backend uri

--- src/backend/axios.js
+++ src/backend/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API_URL = process.env.API_URL || 'http://localhost:8080/'
+const API_URL = process.env.NODE_ENV == 'production' ? 'http://104.248.101.239:8080/' : 'http://localhost:8080/'
 
 export default axios.create({
   baseURL: API_URL,
