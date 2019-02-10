711027a0c087e6f8bfc450c76ca7b231cf81aa3d
Wojciech Mista <wojciech.mista@hotmail.com>
2019-02-10 21:26:12 +0100

    Service worker changes

--- src/service-worker.js
+++ src/service-worker.js
@@ -20,23 +20,3 @@ workbox.routing.registerRoute(
     ],
   }),
 );
-/*
-workbox.routing.registerRoute(
-  new RegExp('https://newsapi.org/v2/top-headlines(.*)'),
-  workbox.strategies.networkFirst({
-    cacheName: 'news-api',
-  }),
-);
-
-workbox.routing.registerRoute(
-  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
-  workbox.strategies.cacheFirst({
-    cacheName: 'googleapis',
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 30,
-      }),
-    ],
-  }),
-);
-*/
