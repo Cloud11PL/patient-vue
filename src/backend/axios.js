967f5d46344094b9d7675f7a524021ba8077492d
Wojciech Mista <wojciech.mista@hotmail.com>
2019-02-17 22:13:34 +0100

    Ngrok url changed

--- src/backend/axios.js
+++ src/backend/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 /**
  * Setting up Axios for API calls.  
  */
-const API_URL = process.env.NODE_ENV == 'production' ? 'https://eea1d239.ngrok.io/' : 'http://localhost:8080/'
+const API_URL = process.env.NODE_ENV == 'production' ? 'https://74c896c0.ngrok.io' : 'http://localhost:8080/'
 
 export default axios.create({
   baseURL: API_URL,
