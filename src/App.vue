83aac5d824dcd012f8ac4dcf4222b45995cd0c90
Wojciech Mista <wojciech.mista@hotmail.com>
2019-04-08 18:12:04 +0200

    added background image

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <router-view />
     <drawerMenu />
   </div>
 </template>
@@ -14,22 +15,33 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Helvetica, sans-serif;
+  height: 100vh;
+  background-image: linear-gradient(
+      to right bottom,
+      rgba(87, 38, 99, 0.5),
+      rgba(2, 1, 4, 0.5)
+    ),
+    url('assets/background.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 0px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  height: 100%;
 }
 </style>
